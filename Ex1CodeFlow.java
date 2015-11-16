59dadbf2066440f0666209408ce88a0b8e35f67b
unknown <gshian01@CSPC114.dcs.bbk.ac.uk>
2015-11-16 14:33:03 +0000

    Exercise 1 Code Flow completed.

---
+++ Ex1CodeFlow.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Ex1CodeFlow {
+	
+	public static void main(String[] args) {
+		Ex1CodeFlow ex1 = new Ex1CodeFlow();
+		ex1.launch(6);
+	}
+	
+	public void launch(int userInput) {
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		intList.add(4);
+		intList.add(5);
+		intList.add(6);
+		try {
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+		} catch (IndexOutOfBoundsException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+}
